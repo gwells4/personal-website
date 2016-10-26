60732c0d0b4fa96c918abec75e75a7e46bd3e3e0
Grant Wells <gwells4@cnm.edu>
2016-10-26 10:27:57 -0600

    Uploaded wireframe images.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8" />
+		<!-- <link rel="Browser-Tab Icon" href="../img/icon-list.svg" type="image/svg"> -->
+		<link rel="apple-touch-icon" sizes="180x180" href="../img/apple-touch-icon.png">
+		<link rel="icon" type="image/png" href="../img/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="../img/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="../img/manifest.json">
+		<link rel="mask-icon" href="../img/safari-pinned-tab.svg" color="#5bbad5">
+		<link rel="shortcut icon" href="../img/favicon.ico">
+		<meta name="msapplication-config" content="../img/browserconfig.xml">
+		<meta name="theme-color" content="#ffffff">
+
+
+		<!-- Add CSS file -->
+		<link href="../css/stylesheet.css" rel="stylesheet" type="text/css" />
+
+		<title>PWP Milestone 2</title>
+
+	</head>
+
+	<body>
+
+		<!-- This is the header -->
+		<header>
+			<h1>PWP Milestone 2</h1>
+		</header>
+
+		<!-- This is the main page content. -->
+		<main>
+
+			<!-- Section 1: Content Strategy -->
+			<section>
+
+				<h2>Purpose</h2>
+
+				<p>The purpose of this site is to serve as a contact point for my professional peers, as well as a means for potential clients and customers to learn more about my skills. To do that, I am including a brief list of projects and skills and a contact form.</p>
+
+			</section>
+
+			<!-- Section 2: Desktop Wireframes -->
+			<section>
+
+				<h3>Desktop Wireframes</h3>
+
+				<img src="../img/personal-website-desktop-wireframe-1.jpg" alt="Desktop Wireframe 1">
+				<img src="../img/personal-website-desktop-wireframe-2.jpg" alt="Desktop Wireframe 2">
+
+			</section>
+
+			<!-- Section 3: Mobile Wireframes -->
+			<section>
+
+				<h3>Mobile Wireframes</h3>
+
+				<img src="../img/personal-website-mobile-wireframe-1.jpg" alt="Mobile Wireframe 1">
+				<img src="../img/personal-website-mobile-wireframe-2.jpg" alt="Mobile Wireframe 2">
+
+			</section>
+
+		</main>
+
+	</body>
+</html>
